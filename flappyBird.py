93325e4ff982f9c4db9da6066bc6bc203e381f77
Jason <jason@orcasmedialab.com>
2024-08-06 19:47:55 -0700

    Added reset button, proper Game Over functionality, and ability to reset game

--- flappyBird.py
+++ flappyBird.py
@@ -33,10 +33,18 @@ pass_pipe = False
 #load images
 bg = pygame.image.load('img/bg.png')
 ground_img = pygame.image.load('img/ground.png')
+button_img = pygame.image.load('img/restart.png')
 
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
+
+def reset_game():
+    pipe_group.empty()
+    flappy.rect.x = 100
+    flappy.rect.y = int(screen_height / 2)
+    score = 0
+    return score
 
 class Bird(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -106,12 +114,36 @@ class Pipe(pygame.sprite.Sprite):
             self.kill()
 
 
+class Button():
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x, y)
+
+    def draw(self):
+        action = False
+
+        #get mouse position
+        pos = pygame.mouse.get_pos()
+        #check if mouse is over button
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1:
+                action = True
+
+        #draw button
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+        return action
+
+
 bird_group = pygame.sprite.Group()
 pipe_group = pygame.sprite.Group()
 
 flappy = Bird(100, int(screen_height / 2))
 bird_group.add(flappy)
 
+#create restart button instance
+button = Button((screen_width // 2) - 50, (screen_height // 2) - 50, button_img)
 
 run = True
 
@@ -169,6 +201,12 @@ while run:
             ground_scroll = 0
 
         pipe_group.update()
+
+    #check for Game Over and reset
+    if gameover == True:
+        if button.draw():
+            gameover = False
+            score = reset_game()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
